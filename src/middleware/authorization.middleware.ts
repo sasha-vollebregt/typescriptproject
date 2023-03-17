567a69845dbbb4a383e60b67c31008cac265e732
Sasha Vollebregt <sasha.vollebregt@outlook.com>
2023-03-17 11:08:39 +0100

    remove node_modules

--- src/middleware/authorization.middleware.ts
+++ src/middleware/authorization.middleware.ts
@@ -1,9 +1,14 @@
 import { expressjwt } from 'express-jwt';
 import { config } from '../config/config.js';
 
-const SECRET_KEY = config.SECRET_KEY || '';
+const SECRET_KEY = config.SECRET_KEY;
+
+if (!SECRET_KEY) {
+  throw new Error('No secret key defined in config');
+}
 
 export const authMiddleware = expressjwt({
   secret: SECRET_KEY,
   algorithms: ['HS256'],
+  getToken: (req) => req.headers.authorization?.split(' ')[1],
 });
--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,22 @@ import {errorHandler} from './middleware/error.middelware.js';
 import { config } from './config/config.js';
 import { logger } from './utils/logger.utils.js';
 
+// Create the Express app
 const app: Express = express();
 const PORT = config.PORT;
 
+// Use the authorization middleware for all routes
 app.use(authMiddleware);
 
+// Define a route for the root URL
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+// Use the error middleware for all routes
 app.use(errorHandler);
 
+// Start the server and listen for incoming requests
 app.listen(PORT, () => {
   logger.info(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
