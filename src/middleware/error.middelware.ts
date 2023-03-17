f5f55d779245cc5b8a9395a3a0908c943ab0d2b4
Sasha Vollebregt <sasha.vollebregt@outlook.com>
2023-03-17 11:17:11 +0100

    added some comments

--- src/middleware/error.middelware.ts
+++ src/middleware/error.middelware.ts
@@ -2,6 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import { logger } from '../utils/logger.utils.js';
 import { CustomError } from 'utils/error.utils.js';
 
+/**
+ * This function is an error middleware that catches custom errors and sends a JSON response to the client.
+ * @param {CustomError} err - The error object passed to the middleware.
+ * @param {Request} req - The Express Request object.
+ * @param {Response} res - The Express Response object.
+ * @param {NextFunction} next - The Express NextFunction object.
+ */
 export const errorHandler = (
   err: CustomError,
   req: Request,
--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -1,10 +1,27 @@
+/**
+ * CustomError class extends the built-in Error class to create custom errors
+ * that can hold a specific status code and a message.
+ */
 export class CustomError extends Error {
   status: number;
+
+   /**
+   * Constructor for the CustomError class.
+   * @param message - A string message that describes the error.
+   * @param status - A number that represents the HTTP status code to return for the error.
+   */
   constructor(message: string, status: number) {
     super(message);
     this.status = status;
     Object.setPrototypeOf(this, CustomError.prototype);
   }
+
+  /**
+   * toJSON method returns a JSON object that represents the error.
+   * It includes a message property with the error message, and a statusCode property
+   * with the HTTP status code for the error.
+   * @returns A JSON object with message and statusCode properties.
+   */
   toJSON() {
     return {
       message: this.message,
